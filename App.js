8230e0d3173080c0d9e2294f6216de0e3c4e3c5d
pradi15 <117429431+pradi15@users.noreply.github.com>
2022-11-15 09:00:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import "./Style.css";
+
+function App() {
+  
+
+  const list = [
+    "Readme",
+    "Apple",
+    "Oneplus",
+    "realme",
+    "appo",
+    "vivo",
+    "sony",
+    "blueberry"
+  ];
+
+  const [filterList, setFilterList] = useState(list);
+
+  const handleSearch = (event) => {
+
+    if (event.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+    const filteredValues = list.filter(
+      (item) =>
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
+  return (
+    <div className="app">
+      <div>
+        Search: <input name="query" type="text" onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => (
+          <div key={index}>{item}</div> //Display each item
+        ))}
+    </div>
+  );
+}
+
+export default App;
